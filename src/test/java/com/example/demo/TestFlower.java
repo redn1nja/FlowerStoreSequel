6a0a9e8b54bf0398f8461a34f34edad92eb549f0
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-01-08 17:00:49 +0200

    tests

---
+++ src/test/java/com/example/demo/TestFlower.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import store.flowers.Flower;
+
+import java.util.Random;
+
+public class TestFlower {
+    private static final Random RANDOM_GENERATOR = new Random();
+    private static final int MAX_PRICE = 100;
+    private Flower flower;
+
+    @BeforeEach
+    public void init() {
+        flower = new Flower();
+    }
+
+    @Test
+    public void testPrice() {
+        int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
+        flower.setPrice(price);
+        Assertions.assertEquals(price, flower.getPrice());
+    }
+
+}
+
